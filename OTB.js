b2999b1e233cf13f25f294fa5997162f6da8f021
Kirstie Davidson <kjd35uk@yahoo.co.uk>
2018-07-13 13:46:16 +0100

    Adding more tests, refactoring function

--- OTB.js
+++ OTB.js
@@ -1,44 +1,43 @@
-// [Ruby hash-keys assigned by hash-rockets can facilitate strings for key-value pairs (e.g. 's' => x) - equivalent to JS key/value pairs]
-
 const orderJobs = jobs => {
   //if passed no jobs, return an empty array
-  if (typeof jobs === "string" && jobs.length === 0) return [];
+  if (jobs.length === 0) return [];
 
-  //otherwise, loop through the jobs list:
+  //otherwise, coerce string into jobs object and loop through:
+  let jobsObj = JSON.parse(jobs);
   let orderedJobs = [];
-  for (let key in jobs) {
+  for (let key in jobsObj) {
     //return an error if a job depends upon itself
-    if (jobs[key] === key) return "Error: a job cannot depend on itself";
+    if (jobsObj[key] === key) return "Error: a job cannot depend on itself";
 
-  //if the job is without dependencies:
-    // if the job is not in final array already, push it.
-    if (jobs[key] === "" && !orderedJobs.includes(key)) orderedJobs.push(key);
-    //if the job is already in final array, skip over it.
-    else if (jobs[key] === "" && orderedJobs.includes(key)) orderedJobs.push();
+    // if a job without dependencies is not in final array already, push it.
+    if (jobsObj[key].length === 0) {
+      if (!orderedJobs.includes(key)) orderedJobs.push(key);
+    }
 
-  //otherwise, if the job has dependecies:
+    //otherwise, for a job with dependencies:
     else {
       //if both key and dependency value are already in final array, return error message
-      if (orderedJobs.includes(jobs[key]) && orderedJobs.includes(key)) {
+      if (orderedJobs.includes(jobsObj[key]) && orderedJobs.includes(key))
         return "Error: the jobs list cannot contain circular dependencies";
-      }
+
       //if neither key nor dependency value are in final array, push both in correct order
-      else if (!orderedJobs.includes(jobs[key]) && !orderedJobs.includes(key)) {
-        orderedJobs.push(jobs[key], key);
+      else if (!orderedJobs.includes(jobsObj[key]) && !orderedJobs.includes(key)) {
+        orderedJobs.push(jobsObj[key], key);
       }
+
       //if key is already in final array, insert dependency value before it
-      else if (orderedJobs.includes(key) && !orderedJobs.includes(jobs[key])) {
-        let keyIndex = orderedJobs.findIndex(job => job === key);
-        orderedJobs.splice(keyIndex, 0, jobs[key]);
+      else if (orderedJobs.includes(key) && !orderedJobs.includes(jobsObj[key])) {
+        let keyIndex = orderedJobs.indexOf(key);
+        orderedJobs.splice(keyIndex, 0, jobsObj[key]);
       }
+
       //if dependency value is already in final array, insert key after it
       else {
-        let valueIndex = orderedJobs.findIndex(job => job === jobs[key]);
+        let valueIndex = orderedJobs.indexOf(jobsObj[key]);
         orderedJobs.splice(valueIndex + 1, 0, key);
       }
     }
   }
   return orderedJobs;
 };
-
 module.exports = { orderJobs };
--- OTB.spec.js
+++ OTB.spec.js
@@ -1,40 +1,109 @@
-const {expect} = require('chai');
-const {orderJobs} = require('../OTB');
+const { expect } = require("chai");
+const { orderJobs } = require("./OTB");
 
-describe('orderJobs' , () => {
-  it('returns an empty array when passed an empty string (no jobs)' , () => {
-    const input = '';
+describe("orderJobs", () => {
+  it("returns an empty array when passed an empty string ie. no jobs", () => {
+    const input = "";
     const actual = orderJobs(input);
     expect(actual).to.eql([]);
   });
-  it('returns an array of one element when passed one job without dependencies' , () => {
-    const input = {'a': ''};
-    const actual = orderJobs(input);
-    expect(actual).to.eql(['a']);
+
+  it("returns an array of one element when passed one job without dependencies", () => {
+    let input = '{"a": ""}';
+    let actual = orderJobs(input);
+    expect(actual).to.eql(["a"]);
+    input = '{"z": ""}';
+    actual = orderJobs(input);
+    expect(actual).to.eql(["z"]);
   });
-  it('returns an array containing all the jobs in any order when passed three jobs without dependencies' , () => {
-    const input = {'a': '', 'b': '', 'c': ''};
-    const actual = orderJobs(input);
-    expect(actual).to.eql(['a', 'b', 'c']);
+
+  it("returns an array of two elements in the correct order when passed one job that has a dependency", () => {
+    let input = '{"a": "b"}';
+    let actual = orderJobs(input);
+    expect(actual).to.eql(["b", "a"]);
+    input = '{"z": "x"}';
+    actual = orderJobs(input);
+    expect(actual).to.eql(["x", "z"]);
   });
-  it('returns an array that contains all three jobs and places a job before any other jobs that depend upon it' , () => {
-    const input = {'a': '', 'b': 'c', 'c': ''};
-    const actual = orderJobs(input);
-    expect(actual).to.eql(['a', 'c', 'b']);
+
+  it("returns an error message when passed one job that depends on itself", () => {
+    let input = '{"a": "a"}';
+    let actual = orderJobs(input);
+    expect(actual).to.equal("Error: a job cannot depend on itself");
+    input = '{"z": "z"}';
+    actual = orderJobs(input);
+    expect(actual).to.equal("Error: a job cannot depend on itself");
   });
-  it('returns an array that contains all jobs and places a job before any other jobs that depend upon it' , () => {
-  const input = {'a': '', 'b': 'c', 'c': 'f', 'd': 'a', 'e': 'b', 'f': ''};
-  const actual = orderJobs(input);
-    expect(actual).to.eql([ 'a', 'd', 'f', 'c', 'b', 'e' ]);
-  });
-  it('returns an error if a job depends on itself' , () => {
-    const input = {'a': '', 'b': '', 'c': 'c'};
+
+  it("returns an array of three elements in any order when passed three jobs without dependencies", () => {
+    const input = '{"a": "", "b": "", "c": ""}';
     const actual = orderJobs(input);
-    expect(actual).to.equal('Error: a job cannot depend on itself');
+    expect(actual).to.be.an("array").that.includes("a", "b", "c");
   });
-  it('returns an error if the jobs list contains circular dependencies' , () => {
-    const input = {'a': '', 'b': 'c', 'c': 'f', 'd': 'a', 'e': '', 'f': 'b'};
+
+  it("returns an array of three elements in the correct order when passed three jobs, some with  dependencies", () => {
+    let input = '{"a": "", "b": "c", "c": ""}';
+    let actual = orderJobs(input);
+    expect(actual).to.eql(["a", "c", "b"]);
+    input = '{"a": "b", "b": "", "c": ""}';
+    actual = orderJobs(input);
+    expect(actual).to.eql(["b", "a", "c"]);
+  });
+
+  it("returns an error message when passed three jobs, including one that depends on itself", () => {
+    let input = '{"a": "", "b": "b", "c": ""}';
+    let actual = orderJobs(input);
+    expect(actual).to.equal("Error: a job cannot depend on itself");
+    input = '{"a": "", "b": "", "c": "c"}';
+    actual = orderJobs(input);
+    expect(actual).to.equal("Error: a job cannot depend on itself");
+  });
+
+  it("returns an error message when passed three jobs that include circular dependencies", () => {
+    let input = '{"a": "b", "b": "a", "c": ""}';
+    let actual = orderJobs(input);
+    expect(actual).to.equal("Error: the jobs list cannot contain circular dependencies");
+    input = '{"a": "", "b": "c", "c": "b"}';
+    actual = orderJobs(input);
+    expect(actual).to.equal("Error: the jobs list cannot contain circular dependencies");
+  });
+
+  it("returns a longer array including all elements in any order when passed a list of many jobs without dependencies", () => {
+    const input = '{"a": "", "b": "", "c": "", "d": "", "e": "", "f": ""}';
     const actual = orderJobs(input);
-    expect(actual).to.equal('Error: the jobs list cannot contain circular dependencies');
+    expect(actual).to.be.an("array").that.includes("a", "b", "c", "d", "e", "f");
+  });
+
+  it("returns an array that contains all jobs in the correct order when passed a list of many jobs", () => {
+    let input = '{"a": "", "b": "c", "c": "f", "d": "a", "e": "b", "f": ""}';
+    let actual = orderJobs(input);
+    expect(actual).to.eql(["a", "d", "f", "c", "b", "e"]);
+    input = '{"a": "b", "b": "c", "c": "f", "d": "a", "e": "b", "f": ""}';
+    actual = orderJobs(input);
+    expect(actual).to.eql(["f", "c", "b", "e", "a", "d"]);
+    input = '{"a": "b", "b": "c", "c": "", "d": "a", "e": "b", "f": "c"}';
+    actual = orderJobs(input);
+    expect(actual).to.eql(["c", "f", "b", "e", "a", "d"]);
+  });
+
+  it("returns an error message when passed a list of many jobs, including jobs that depend on themselves", () => {
+    let input = '{"a": "a", "b": "", "c": "", "d": "", "e": "", "f": ""}';
+    let actual = orderJobs(input);
+    expect(actual).to.equal("Error: a job cannot depend on itself");
+    input = '{"a": "", "b": "", "c": "c", "d": "d", "e": "", "f": ""}';
+    actual = orderJobs(input);
+    expect(actual).to.equal("Error: a job cannot depend on itself");
+    input = '{"a": "", "b": "", "c": "", "d": "", "e": "e", "f": "f"}';
+    actual = orderJobs(input);
+    expect(actual).to.equal("Error: a job cannot depend on itself");
+  });
+
+  it("returns an error message when passed a list of many jobs that contains circular dependencies", () => {
+    let input = '{"a": "", "b": "c", "c": "f", "d": "a", "e": "", "f": "b"}';
+    let actual = orderJobs(input);
+    expect(actual).to.equal("Error: the jobs list cannot contain circular dependencies");
+    input = '{"a": "", "b": "c", "c": "f", "d": "a", "e": "", "f": "c"}';
+    actual = orderJobs(input);
+    expect(actual).to.equal("Error: the jobs list cannot contain circular dependencies");
   });
 });
